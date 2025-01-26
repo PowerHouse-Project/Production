e04b1704694b9bd1f1270a273d65e9f3282f65e4
Aditya Shibu <as2397@hw.ac.uk>
2025-01-26 10:18:38 +0400

    Add Bootstrap CSS and update dependencies; center header text

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-danger">Hello bootstrap</h1>
+      <h1 className="text-danger text-center">Hello bootstrap</h1>
     </main>
   );
 }
